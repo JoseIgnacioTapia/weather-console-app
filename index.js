7665bac7bbc61b2a03d8ff76fc2cc17efe845bfd
JoseIgnacioTapia <josignaciot@gmail.com>
2023-07-19 22:58:50 -0300

    Model to control the application

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 import { leerInput, inquireMenu, pausa } from "./helpers/inquirer.js";
+import { Busquedas } from "./models/busquedas.js";
 
 const main = async () => {
+  const busquedas = new Busquedas();
   let opt;
 
   do {
     opt = await inquireMenu();
-    console.log({ opt });
+
+    switch (opt) {
+      case 1:
+        // Mostrar mensaje
+        const lugar = await leerInput("Ciudad: ");
+        console.log(lugar);
+
+        // Buscar los lugares
+
+        // Seleccionar el lugar
+
+        // Clima
+
+        // Mostrar resultados
+        console.log("\nInformación de la ciudad\n".green);
+        console.log("Ciudad:");
+        console.log("Lat:");
+        console.log("Lng:");
+        console.log("Temperatura:");
+        console.log("Mínima:");
+        console.log("Máxima:");
+
+        break;
+
+      default:
+        break;
+    }
 
     if (opt !== 0) await pausa();
   } while (opt !== 0);
---
+++ models/busquedas.js
@@ -0,0 +1,14 @@
+export class Busquedas {
+  historial = ["Tegucigalpa", "Madrid", "San José"];
+
+  constructor() {
+    // TODO: leer DB si existe
+  }
+
+  async ciudad(lugar = "") {
+    // peticion thhp
+    console.log(lugar);
+
+    return []; // Retornar los lugares
+  }
+}
