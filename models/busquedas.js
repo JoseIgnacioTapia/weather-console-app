55d65597ed4365fa803776fa96c4d6a68e232431
JoseIgnacioTapia <josignaciot@gmail.com>
2023-07-28 23:10:52 -0300

    Create instances of axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -5,10 +5,31 @@ export class Busquedas {
     // TODO: leer DB si existe
   }
 
+  get paramsMapbox() {
+    return {
+      acces_token:
+        "pk.eyJ1Ijoiam9laXQiLCJhIjoiY2xrYnYwNzRrMGlsOTNmb2Jxc3IwcnhkNyJ9.PgUvdbkOlDh6KsQ1iVb3NQ",
+      limit: 5,
+      language: "es",
+    };
+  }
+
   async ciudad(lugar = "") {
+    try {
+      // peticion http
+      const intance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapbox,
+      });
+
+      const resp = await intance.get();
+      console.log(resp.data);
+
+      return [];
+    } catch (error) {
+      return [];
+    }
     // peticion thhp
     console.log(lugar);
-
-    return []; // Retornar los lugares
   }
 }
